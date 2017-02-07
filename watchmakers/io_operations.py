ac5c5eebec85ee5747d92c02e0d5189b04f2d715
MarcFBergevin <marcbergevin@gmail.com>
2017-02-07 10:49:29 -0800

    Adding a way to ntuple all files

--- watchmakers/io_operations.py
+++ watchmakers/io_operations.py
@@ -589,5 +589,7 @@ def extractNtupleALL(arguments):
                 for idx,cover in enumerate(coverage):
                     for run in range(N):
                         fIn =  "root_files/%s/%s/watchman_%s_%s_%s_%d.root" %(ii,cover,ii,cover,loc[j],run)
-                        directoryOut = "ntuple_root_files%s/%s/%s/watchman_%s_%s_%s_%d.root" %(additionalString,ii,cover,ii,cover,loc[j],run)
-                        print fIn, " -> ", directoryOut
+                        fOut = "ntuple_root_files%s/%s/%s/watchman_%s_%s_%s_%d.root" %(additionalString,ii,cover,ii,cover,loc[j],run)
+                        print fIn, " -> ", fOut
+                        goldenFileExtractor(fIn,minNHIT,goodness,dirGoodness,timemask,\
+                                                rate,distancemask,fidV,pmtV,tankV,fOut)
