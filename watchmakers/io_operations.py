1ccb62b83972568bd03c10a5a6e0bd01ab655641
MarcFBergevin <marcbergevin@gmail.com>
2018-01-23 10:55:25 -0800

    Debugging code on BORAX, but developping code on laptop, hence many changes.

--- watchmakers/io_operations.py
+++ watchmakers/io_operations.py
@@ -854,7 +854,7 @@ def performPass1(arguments):
     for j in range(len(iso)):
         for ii in d["%s"%(iso[int(j)])]:
             for idx,cover in enumerate(coverage):
-                _str = "JOB_pass1_%s%s%s.sh" %(additionalString,ii,cover)
+                _str = "job/JOB_pass1_%s%s%s.sh" %(additionalString,ii,cover)
                 outfile.writelines('msub %s\n'%(_str))
                 outfile2 = open(_str,"wb")
                 outfile2.writelines("""#!/bin/sh
