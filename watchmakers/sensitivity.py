d026c95e502bce37cec14c05248824ae5d50a1d3
MarcFBergevin <marcbergevin@gmail.com>
2018-03-14 10:57:01 -0700

    Developping Accidental evaluation function.

--- watchmakers/sensitivity.py
+++ watchmakers/sensitivity.py
@@ -661,7 +661,7 @@ def sensitivityMapPass2():
     print ' boulby rates: %4.2f per %s per %4.2f kton; [r: %4.2f m; z: %4.2f m]'\
     %(boulbyRate,t,sizeDetc,fidRadius/1000.,fidHeight/1000.)
     #fast neutrons
-
+    print 'Debug',rates["boulby_S"],FVkTonRatio
     print '\nEvaluation not based on geoneutrinos.org'
     detectorMedium,detectorMass,reactorPower,reactorStandoff = 1,sizeDetc*1000.,1.5,24.98
     experiment = nuOsc.NeutrinoOscillation(detectorMedium,detectorMass,reactorPower,reactorStandoff)
