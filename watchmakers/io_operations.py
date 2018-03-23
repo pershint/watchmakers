31f18a9c865c75d78cd77111401e080aa0879688
MarcFBergevin <marcbergevin@gmail.com>
2018-03-23 08:58:26 -0700

    Developping Accidental evaluation function.

--- watchmakers/io_operations.py
+++ watchmakers/io_operations.py
@@ -666,9 +666,9 @@ def generateJobsNew(N,arguments):
                         dir = "jobs%s/%s/%s/%s/%s"%(additionalMacStr,_cover,_loc,_element,_p)
                         outfile = open(dir+'/job%08d.sh'%(i*10),"wb")
                         for _j in range(10):
-                            mac = "macro%s/%s/%s/%s/%s/run%08d/run%08d.mac"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
-                            r_outfile = "root_file%s/%s/%s/%s/%s/run%08d/run%08d.root"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
-                            b_outfile = "bonsai_root_file%s/%s/%s/%s/%s/run%08d/run%08d.root"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
+                            mac = "macro%s/%s/%s/%s/%s/run%08d/run_%08d.mac"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
+                            r_outfile = "root_file%s/%s/%s/%s/%s/run%08d/run_%08d.root"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
+                            b_outfile = "bonsai_root_file%s/%s/%s/%s/%s/run%08d/run_%08d.root"%(additionalMacStr,_cover,_loc,_element,_p,i*10,i*10+_j)
                             lines = ''' rat %s -o %s
 bonsai %s %s\n'''%(mac,r_outfile,r_outfile,b_outfile)
 
