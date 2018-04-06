cba1cad4cbc678033c1a72b6680a151b05bd991d
MarcFBergevin <marcbergevin@gmail.com>
2018-04-06 14:24:34 -0700

    Developping Accidental evaluation function.

--- watchmakers/sensitivity.py
+++ watchmakers/sensitivity.py
@@ -1333,7 +1333,7 @@ def readEfficiencyHistogram():
 
 
             print 'Offset:',offset,',Found max S/sqrt(S+B)',_maxSoverB,',(S,B,n9,dtw):(',_maxSignal,_maxBkgd,_maxOffn9,_maxOff_dtw,')'
-            line += _line + _line2,
+            line += (_line + _line2,)
 
     print line
     f_root = TFile(_str,"recreate")
