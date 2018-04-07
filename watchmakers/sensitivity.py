c77ae391d9bf1ab5c73e223e81318d10b043bd23
MarcFBergevin <marcbergevin@gmail.com>
2018-04-07 09:54:51 -0700

    Developping Accidental evaluation function.

--- watchmakers/sensitivity.py
+++ watchmakers/sensitivity.py
@@ -1353,7 +1353,7 @@ def readEfficiencyHistogram():
             print _l[i],
         print ''
 
-    _res = "%s %s %4.1f %3d %3d %4.3f %4.3f" %(arguments['--shieldThick'],_cover,_maxOff_dtw2,_maxOffn92,_maxOffn92-_maxOffset2,_maxSignal2,_maxBkgd2)
+    _res = "%s %4.1f %3d %3d %4.3f %4.3f" %(_cover,_maxOff_dtw2,_maxOffn92,_maxOffn92-_maxOffset2,_maxSignal2,_maxBkgd2)
     _strRes = "results_DTW_%dmm_U238_%4.3fPPM_Th232_%4.3fPPM_K_%4.3fPPM.txt"%(float(arguments['--shieldThick']),float(arguments["--U238_PPM"]),float(arguments["--Th232_PPM"]),float(arguments["--K_PPM"]))
     # _strRes = 'res.txt'
     with open(_strRes,'a') as file:
