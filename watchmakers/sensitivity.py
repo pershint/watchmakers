e2ab05310be4d2595ff0e276025dd8bdb55001b2
MarcFBergevin <marcbergevin@gmail.com>
2018-04-10 16:10:09 -0700

    Developping Accidental evaluation function.

--- watchmakers/sensitivity.py
+++ watchmakers/sensitivity.py
@@ -1389,8 +1389,11 @@ def readEfficiencyHistogram():
     _B = _maxSignal2*cut_signal*1.15 + _maxBkgd2 # Includes a 15% other reactor component
     OnOffRatio = 1.5
 
-    T3SIGMA = sigma**2*(_B +(_S+_B)/OnOffRatio)/_S/_S
+    if _S >0:
+        T3SIGMA = sigma**2*(_B +(_S+_B)/OnOffRatio)/_S/_S
 
+    else:
+        T3SIGMA = 1e999    
     metric = T3SIGMA*OnOffRatio + T3SIGMA
     _res = "%s %4.1f %3d %3d %4.3f %4.3f %4.1f %4.1f" %(_cover,_maxOff_dtw2,_maxOffn92,_maxOffn92-_maxOffset2,_maxSignal2,_maxBkgd2,T3SIGMA,metric)
     _strRes = "results_DTW_%dmm_U238_%4.3fPPM_Th232_%4.3fPPM_K_%4.3fPPM.txt"%(float(arguments['--shieldThick']),float(arguments["--U238_PPM"]),float(arguments["--Th232_PPM"]),float(arguments["--K_PPM"]))
